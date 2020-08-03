1eadc08cc5c705e0c59ddde87f94594dbe9bdee8
Abbasleader <abbasjaffary33@gmail.com>
2020-08-03 22:38:22 +0430

    video codes and tested

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Search bar',
+      title: 'Search Bar',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
   }
@@ -41,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     controller.addListener(() {
       setState(() {
@@ -58,59 +57,54 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               Expanded(
                 child: Container(
+                  height: 80,
+                  width: double.infinity,
                   padding: EdgeInsets.only(
-                    bottom: 5,
                     top: 35,
                     left: 15,
                     right: 5,
+                    bottom: 5,
                   ),
-                  height: 80,
-                  width: double.infinity,
                   color: Color.fromRGBO(221, 221, 221, 1),
                   child: TextField(
                     controller: controller,
                     autofocus: false,
                     decoration: InputDecoration(
+                      labelText: 'Search Something',
                       filled: true,
                       fillColor: Colors.white,
-                      labelText: 'Search something',
-                      contentPadding: EdgeInsets.all(10),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(25.7),
-                      ),
                       enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
                         borderRadius: BorderRadius.circular(25.7),
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25.7),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                     ),
                   ),
                 ),
               ),
               Container(
-                color: Color.fromRGBO(221, 221, 221, 1),
-                padding: EdgeInsets.only(top: 25, right: 15),
                 height: 80,
-                child: Icon(
-                  Icons.search,
-                  color: Colors.black,
-                  size: 25,
+                color: Color.fromRGBO(221, 221, 221, 1),
+                padding: EdgeInsets.only(
+                  top: 25,
+                  right: 15,
                 ),
-              ),
+                child: Icon(Icons.search),
+              )
             ],
           ),
           Expanded(
             child: ListView.builder(
               itemCount: list.length,
               itemBuilder: (ctx, index) {
-                return list[index]
-                        .toString()
-                        .toLowerCase()
-                        .contains(filter.toLowerCase())
+                return list[index].toLowerCase().contains(filter.toLowerCase())
                     ? Padding(
                         padding: const EdgeInsets.symmetric(
                           horizontal: 22.0,
-                          vertical: 10,
+                          vertical: 10.0,
                         ),
                         child: Text(
                           list[index],
@@ -120,9 +114,128 @@ class _MyHomePageState extends State<MyHomePage> {
                     : SizedBox.shrink();
               },
             ),
-          ),
+          )
         ],
       ),
     );
   }
 }
+
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(MyApp());
+//
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Search Bar',
+//      debugShowCheckedModeBanner: false,
+//      theme: ThemeData(primarySwatch: Colors.blue),
+//      home: MyHomePage(),
+//    );
+//  }
+//}
+//
+//class MyHomePage extends StatefulWidget {
+//  @override
+//  _MyHomePageState createState() => _MyHomePageState();
+//}
+//
+//class _MyHomePageState extends State<MyHomePage> {
+//  TextEditingController controller = new TextEditingController();
+//  String filter = '';
+//  List<String> list = [
+//    'William',
+//    'James',
+//    'Harper',
+//    'Mason',
+//    'Evelyn',
+//    'Ella',
+//    'Avery',
+//    'Jackson',
+//    'John',
+//    'Bob',
+//    'Bale',
+//  ];
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    controller.addListener(() {
+//      setState(() {
+//        filter = controller.text;
+//      });
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: Column(
+//        children: <Widget>[
+//          Row(
+//            children: <Widget>[
+//              Expanded(
+//                child: Container(
+//                  height: 80,
+//                  padding: EdgeInsets.only(
+//                    top: 35,
+//                    left: 15,
+//                    right: 5,
+//                    bottom: 5,
+//                  ),
+//                  width: double.infinity,
+//                  color: Color.fromRGBO(221, 221, 221, 1),
+//                  child: TextField(
+//                    controller: controller,
+//                    autofocus: false,
+//                    decoration: InputDecoration(
+//                      filled: true,
+//                      fillColor: Colors.white,
+//                      labelText: 'Search Something',
+//                      contentPadding: EdgeInsets.all(10),
+//                      focusedBorder: OutlineInputBorder(
+//                        borderRadius: BorderRadius.circular(25.7),
+//                        borderSide: BorderSide(color: Colors.white),
+//                      ),
+//                      enabledBorder: UnderlineInputBorder(
+//                        borderRadius: BorderRadius.circular(25.7),
+//                        borderSide: BorderSide(color: Colors.white),
+//                      ),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              Container(
+//                height: 80,
+//                color: Color.fromRGBO(221, 221, 221, 1),
+//                padding: EdgeInsets.only(top: 25, right: 15),
+//                child: Icon(Icons.search),
+//              ),
+//            ],
+//          ),
+//          Expanded(
+//            child: ListView.builder(
+//              itemCount: list.length,
+//              itemBuilder: (ctx, index) {
+//                return list[index].toLowerCase().contains(filter.toLowerCase())
+//                    ? Padding(
+//                        padding: const EdgeInsets.symmetric(
+//                          horizontal: 22.0,
+//                          vertical: 10.0,
+//                        ),
+//                        child: Text(
+//                          list[index],
+//                          style: TextStyle(fontSize: 15.0),
+//                        ),
+//                      )
+//                    : SizedBox.shrink();
+//              },
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
